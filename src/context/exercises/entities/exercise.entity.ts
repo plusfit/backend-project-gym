bfd0ad7e7d60e9c52dd4cac358a193d170cd3749
Nico Gilardoni <nicogilardonik@gmail.com>
2024-08-26 08:11:01 -0300

    feat: :sparkles: Exercise entity with additional properties and constructor

--- src/context/exercises/entities/exercise.entity.ts
+++ src/context/exercises/entities/exercise.entity.ts
@@ -1,1 +1,16 @@
-export class Exercise {}
+export class Exercise {
+  id?: string;
+  name?: string;
+  description?: string;
+  gifUrl?: string;
+  type?: string;
+  mode?: string;
+  minutes?: number;
+  rest?: number;
+  reps?: number;
+  series?: number;
+
+  constructor(partial: Partial<Exercise>) {
+    Object.assign(this, partial);
+  }
+}
