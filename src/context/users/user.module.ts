6f0cd9959117f7ef5b7af4db3225a690f28a9c88
Nico Gilardoni <nicogilardonik@gmail.com>
2024-08-26 15:23:41 -0300

    feat: :sparkles: Add UsersService to UserModule

--- src/context/users/user.module.ts
+++ src/context/users/user.module.ts
@@ -2,7 +2,10 @@ import { Module } from "@nestjs/common";
 
 import { UsersController } from "@/src/context/users/users.controller";
 
+import { UsersService } from "./users.service";
+
 @Module({
   controllers: [UsersController],
+  providers: [UsersService],
 })
 export class UserModule {}
