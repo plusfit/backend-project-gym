9ac0640fd3c900e2c4cfcdc5773245f537880df6
Nico Gilardoni <nicogilardonik@gmail.com>
2024-08-30 13:32:51 -0300

    feat: :sparkles: Update Client entity with UserInfo class

--- src/context/clients/entities/client.entity.ts
+++ src/context/clients/entities/client.entity.ts
@@ -1,8 +1,16 @@
+export class UserInfo {
+  name!: string;
+  age!: number;
+
+  constructor(partial: Partial<UserInfo>) {
+    Object.assign(this, partial);
+  }
+}
 export class Client {
   type!: string;
   planId!: string;
   email!: string;
-  userInfo!: any;
+  userInfo!: UserInfo;
 
   constructor(partial: Partial<Client>) {
     Object.assign(this, partial);
