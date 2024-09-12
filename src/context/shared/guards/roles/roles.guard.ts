bf705de3be997ac5cfa12b8872b871f770b4364c
Nico Gilardoni <nicogilardonik@gmail.com>
2024-09-12 09:34:46 -0300

    feat: :sparkles: RoleGuard created

--- src/context/shared/guards/roles/roles.guard.ts
+++ src/context/shared/guards/roles/roles.guard.ts
@@ -3,33 +3,83 @@ import {
   ExecutionContext,
   ForbiddenException,
   Injectable,
+  UnauthorizedException,
 } from "@nestjs/common";
+import { ConfigService } from "@nestjs/config";
 import { Reflector } from "@nestjs/core";
+import jwt from "jsonwebtoken";
 
 import { ROLES_KEY } from "@/src/context/shared/guards/roles/roles.decorator";
 
 @Injectable()
 export class RolesGuard implements CanActivate {
-  constructor(private readonly reflector: Reflector) {}
+  constructor(
+    private readonly reflector: Reflector,
+    private readonly configService: ConfigService,
+  ) {}
 
   canActivate(context: ExecutionContext): boolean {
-    const requiredRoles = this.reflector.getAllAndOverride<string[]>(
-      ROLES_KEY,
-      [context.getHandler(), context.getClass()],
-    );
+    const requiredRoles = this.getRequiredRoles(context);
 
     if (!requiredRoles) {
-      return true;
+      return true; // Si no se requieren roles específicos, permite el acceso
     }
 
-    const request = context.switchToHttp().getRequest();
-    const user = request.user;
+    const req = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(req.headers["authorization"]);
+    const decoded = this.verifyToken(token);
 
-    if (!user) {
-      throw new ForbiddenException("User not found");
+    const userRole = decoded?.role; //del token obtengo el rol del usuario
+
+    if (!userRole) {
+      throw new ForbiddenException("User role not found");
     }
 
-    const hasRole = () => requiredRoles.some(role => user.role?.includes(role));
-    return user && hasRole();
+    const hasRole = this.userHasRequiredRole(userRole, requiredRoles);
+
+    if (!hasRole) {
+      throw new ForbiddenException("User does not have required role");
+    }
+    return true;
+  }
+
+  private getRequiredRoles(context: ExecutionContext): string[] | undefined {
+    return this.reflector.getAllAndOverride<string[]>(ROLES_KEY, [
+      context.getHandler(),
+      context.getClass(),
+    ]);
+  }
+
+  private extractTokenFromHeader(authHeader: string | undefined): string {
+    if (!authHeader) {
+      throw new UnauthorizedException("Authorization header not found");
+    }
+
+    const token = authHeader.split(" ")[1];
+    if (!token) {
+      throw new UnauthorizedException("Token not found");
+    }
+
+    return token;
+  }
+
+  private verifyToken(token: string): any {
+    const secret = this.configService.get<string>("JWT_ACCESS_SECRET");
+    if (!secret) {
+      throw new UnauthorizedException("JWT secret is not set");
+    }
+
+    try {
+      return jwt.verify(token, secret);
+    } catch {
+      throw new UnauthorizedException("Invalid token");
+    }
+  }
+
+  private userHasRequiredRole(
+    userRole: string,
+    requiredRoles: string[],
+  ): boolean {
+    return requiredRoles.includes(userRole);
   }
 }
