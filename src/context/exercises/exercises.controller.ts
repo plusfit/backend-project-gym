9c6e09dfe7bd62eb91f378352d410cc26a083ce8
nahuelgilvangwe <nahuel@vangwe.com>
2024-11-10 18:11:15 -0300

    feat: changes at clients, plans, routine and subroutine crated

--- src/context/exercises/exercises.controller.ts
+++ src/context/exercises/exercises.controller.ts
@@ -7,15 +7,15 @@ import {
   Patch,
   Post,
   Query,
-  UseGuards,
+  // UseGuards,
 } from "@nestjs/common";
 import { ApiTags } from "@nestjs/swagger";
 
 import { FiltersDto } from "@/src/context/exercises/dto/filters.dto";
-import { Role } from "@/src/context/shared/constants/roles.constant";
-import { PageDto } from "@/src/context/shared/dtos/page.dto";
-import { Roles } from "@/src/context/shared/guards/roles/roles.decorator";
-import { RolesGuard } from "@/src/context/shared/guards/roles/roles.guard";
+// import { Role } from "@/src/context/shared/constants/roles.constant";
+// import { PageDto } from "@/src/context/shared/dtos/page.dto";
+// import { Roles } from "@/src/context/shared/guards/roles/roles.decorator";
+// import { RolesGuard } from "@/src/context/shared/guards/roles/roles.guard";
 
 import { CreateExerciseDto } from "./dto/create-exercise.dto";
 import { UpdateExerciseDto } from "./dto/update-exercise.dto";
@@ -27,15 +27,15 @@ export class ExercisesController {
   constructor(private readonly exercisesService: ExercisesService) {}
 
   @Post()
-  @Roles(Role.Admin)
-  @UseGuards(RolesGuard)
+  // @Roles(Role.Admin)
+  // @UseGuards(RolesGuard)
   create(@Body() createExerciseDto: CreateExerciseDto) {
     return this.exercisesService.create(createExerciseDto);
   }
 
   @Get()
-  @Roles(Role.Admin)
-  @UseGuards(RolesGuard)
+  // @Roles(Role.Admin)
+  // @UseGuards(RolesGuard)
   findAll(@Query() pageDto: PageDto, @Query() filtersDto: FiltersDto) {
     return this.exercisesService.getExercises(
       pageDto.page,
@@ -47,15 +47,15 @@ export class ExercisesController {
   }
 
   @Get(":id")
-  @Roles(Role.Admin, Role.Client)
-  @UseGuards(RolesGuard)
+  // @Roles(Role.Admin, Role.Client)
+  // @UseGuards(RolesGuard)
   findOne(@Param("id") id: string) {
     return this.exercisesService.findOne(id);
   }
 
   @Patch(":id")
-  @Roles(Role.Admin)
-  @UseGuards(RolesGuard)
+  // @Roles(Role.Admin)
+  // @UseGuards(RolesGuard)
   update(
     @Param("id") id: string,
     @Body() updateExerciseDto: UpdateExerciseDto,
@@ -64,8 +64,8 @@ export class ExercisesController {
   }
 
   @Delete(":id")
-  @Roles(Role.Admin)
-  @UseGuards(RolesGuard)
+  // @Roles(Role.Admin)
+  // @UseGuards(RolesGuard)
   remove(@Param("id") id: string) {
     return this.exercisesService.remove(id);
   }
---
+++ src/context/routines/services/routines.service.ts
@@ -0,0 +1,106 @@
+import {
+  HttpException,
+  Inject,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
+
+import { CreateRoutineDto } from "@/src/context/routines/dto/create-routine.dto";
+import {
+  ROUTINE_REPOSITORY,
+  SUB_ROUTINE_REPOSITORY,
+} from "@/src/context/routines/repositories/mongo-sub-routine.repository";
+
+@Injectable()
+export class RoutinesService {
+  constructor(
+    @Inject(SUB_ROUTINE_REPOSITORY)
+    private readonly subRoutineRepository: any,
+    @Inject(ROUTINE_REPOSITORY)
+    private readonly routineRepository: any,
+  ) {}
+
+  async createRoutine(createRoutineDto: CreateRoutineDto) {
+    for (const subRoutineId of createRoutineDto.subRoutines) {
+      if (!subRoutineId) {
+        throw new HttpException("Exercise ID is required", 500);
+      }
+      const subRoutineExist =
+        await this.subRoutineRepository.findOne(subRoutineId);
+      if (!subRoutineExist) {
+        throw new NotFoundException(
+          // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+          `Exercise with ID ${subRoutineId} not found`,
+        );
+      }
+    }
+    return this.routineRepository.createRoutine(createRoutineDto);
+  }
+
+  async deleteRoutine(id: string) {
+    const routine = await this.routineRepository.findById(id);
+    if (!routine) {
+      throw new NotFoundException(`Routine with ID ${id} not found`);
+    }
+    return this.routineRepository.deleteRoutine(id);
+  }
+  async updateRoutine(routineId: string, updateData: any, clientId?: string) {
+    const routine = await this.routineRepository.findById(routineId);
+
+    if (!routine) {
+      throw new Error("Routine not found");
+    }
+
+    if (!routine.isCustom && clientId) {
+      routine.isCustom = true;
+      const newRoutine = {
+        ...routine.toObject(),
+        ...updateData,
+        isCustom: false,
+      };
+
+      return await this.routineRepository.createRoutine(newRoutine);
+
+      //TODO: // Asignar la nueva rutina al cliente
+      // await this.clientRepository.addRoutineToClient(
+      //   clientId,
+      //   savedRoutine._id,
+      // ); // Check if this will be in the frontend
+    } else {
+      return this.routineRepository.updateRoutine(routineId, updateData);
+    }
+  }
+
+  getRoutineById(id: string) {
+    return this.routineRepository.findOne(id);
+  }
+
+  async getRoutines(
+    page: number,
+    limit: number,
+    name?: string,
+    type?: string,
+    mode?: string,
+  ) {
+    const offset = (page - 1) * limit;
+    const filters: any = {};
+
+    if (name) {
+      filters.name = { $regex: name, $options: "i" };
+    }
+
+    if (type) {
+      filters.type = type;
+    }
+
+    if (mode) {
+      filters.mode = mode;
+    }
+
+    const [data, total] = await Promise.all([
+      this.routineRepository.getRoutines(offset, limit, filters),
+      this.routineRepository.countRoutines(filters),
+    ]);
+    return { data, total, page, limit };
+  }
+}
