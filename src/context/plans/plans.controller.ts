4f6cfe415bf30dbb4a414cc8659b89732b4db019
Angel Gomez <106106151+AngelGomez12@users.noreply.github.com>
2025-03-24 16:04:58 -0300

    fix: :bug: fix: :bug: feat: :sparkles: feat: Add optional hourId field

--- src/context/plans/plans.controller.ts
+++ src/context/plans/plans.controller.ts
@@ -82,6 +82,7 @@ export class PlansController {
       getClientsAssignalDto.email,
       getClientsAssignalDto.name,
       getClientsAssignalDto.CI,
+      getClientsAssignalDto.hourId,
     );
   }
 
